ad606a454df39b4cf1e5662836e73eec963fa126
Steven R. Loomis <srl295@gmail.com>
2023-01-25 13:23:16 -0600

    fix: deal with large payload from spawn. also truncate some troublesome fields that would be too big

--- lib/plugin-taskw.ts
+++ lib/plugin-taskw.ts
@@ -20,13 +20,18 @@ function fetchData(config: any) {
         const taskw = spawn(getTaskCommand(), getTaskArgs());
         let msg = '';
         let err = '';
-        taskw.stdout.on('data', (data : any) => (msg = data.toString('utf-8')));
+        taskw.stdout.on('data', (data : any) => (msg = msg + data.toString('utf-8')));
         taskw.stderr.on('data', (data : any) => console.error(data.toString('utf-8')));
         taskw.on('close', (code : number) => {
             if((code !== 0)) {
                 return reject(Error('Taskw: ' + (err || 'non zero exit')));
             }
-            return resolve(JSON.parse(msg));
+            const j = JSON.parse(msg);
+            // this one is too big
+            delete (j || [])[0].githubbody;
+            delete (j || [])[0].annotations;
+
+            return resolve(j);
         });
     });
 }
